727fe3528b1831dfd68caf37f42723d7e81bc96c
Joao Pedro <jpcsnoberto@gmail.com>
2024-11-25 20:41:46 -0300

    feat: search functionality in search bar component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { SearchBarComponent } from "../search-bar/search-bar.component";
 import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchBarComponent, ButtonComponent],
+  imports: [SearchBarComponent, ButtonComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ReactiveFormsModule],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
+  queryControl = new FormControl('');
+
+  constructor(private router: Router) {}
+
+  searchVideo(event: Event) {
+    event.preventDefault();
+    this.router.navigate([`/search-video/${this.queryControl.value}`]);
+    this.queryControl.reset();
+  }
 
 }
