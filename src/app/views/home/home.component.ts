c76112deae8bb1b59f7f46db99b5bddcc8908536
Joao Pedro <jpcsnoberto@gmail.com>
2024-11-25 17:37:47 -0300

    feat: home view base structure and styling; updated routing

---
+++ src/app/views/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { VideoCardComponent } from "../../components/video-card/video-card.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [VideoCardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
