32de088e85abd871293a390d065f20ac124e946d
Joao Pedro <jpcsnoberto@gmail.com>
2024-11-28 22:07:43 -0300

    feat: enhance video card styling and functionality; implement dynamic video loading and shuffling

--- src/app/views/watch-video/watch-video.component.ts
+++ src/app/views/watch-video/watch-video.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { VideoService } from '../../services/video/video.service';
+
+import { Video } from '../../types/models';
+
 import { VideoCardComponent } from "../../components/video-card/video-card.component";
 
 @Component({
@@ -9,5 +13,25 @@ import { VideoCardComponent } from "../../components/video-card/video-card.compo
   styleUrl: './watch-video.component.css'
 })
 export class WatchVideoComponent {
+  videos: Video[] = [];
 
+  constructor(private videoService: VideoService) {}
+
+  ngOnInit() {
+    this.loadVideos();
+  }
+
+  loadVideos() {
+    return this.videoService.getVideos().subscribe(videos => {
+      this.videos = this.shuffleVideos(videos);
+    });
+  }
+
+  shuffleVideos(videos: Video[]) {
+    for (let i = videos.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [videos[i], videos[j]] = [videos[j], videos[i]];
+    }
+    return videos
+  }
 }
