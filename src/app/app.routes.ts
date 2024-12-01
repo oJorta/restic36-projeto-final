baa15848b95786ae3ab418e2dc41a4bb15d4df83
Joao Pedro <jpcsnoberto@gmail.com>
2024-11-30 22:09:04 -0300

    feat: add 404 page not found component; added videos to db.json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { SearchVideoComponent } from './views/search-video/search-video.component';
 import { WatchVideoComponent } from './views/watch-video/watch-video.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'search-video', component: SearchVideoComponent },
   { path: 'watch-video/:id', component: WatchVideoComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
